eeaf9875119fb7ecd160cab338cac4da157542df
Abdullah Zahid <abdullahzahid1796@gmail.com>
2025-01-18 18:38:07 +0500

    Parcel Delivery Modified by adding delivery status

--- frontend/src/components/OpenCompartment.jsx
+++ frontend/src/components/OpenCompartment.jsx
@@ -6,7 +6,7 @@ function OpenCompartment(props) {
     const [compstateid, setCompStateID] = React.useState();
     const [parcelid, setParcelID] = React.useState();
     const [purpose, setPurpose] = React.useState();
-    
+
     React.useEffect(() => {
         setOTP(props.otp);
         setCompStateID(props.compstateid);
@@ -29,7 +29,6 @@ function OpenCompartment(props) {
         await axios.put('http://localhost:4002/Locker/Compartment/compstateid', values)
             .then(response => {
                 console.log(response.data);
-                //res.status(200).send({message: "Locker reserved"});
             })
             .catch(err => {
                 console.error("Error while placing parcel: " + err);
@@ -46,10 +45,21 @@ function OpenCompartment(props) {
         await axios.put('http://localhost:4002/Locker/Compartment/otp', values1)
             .then(response => {
                 console.log(response.data);
-                //res.status(200).send({message: "Locker reserved"});
             })
             .catch(err => {
                 console.error("Error while placing parcel: " + err);
+            });
+
+        const values2 = {
+            parcelID: parcelid,
+            status: "parcelPlaced"
+        }
+        await axios.put('http://localhost:4001/updateStatus', values2)
+            .then(response => {
+                console.log(response.data.message);
+            })
+            .catch(err => {
+                throw (err);
             });
 
         //sending SMS to receiver for new OTP to take parcel
@@ -57,18 +67,15 @@ function OpenCompartment(props) {
         alert("Parcel has been placed successfully");
     }
 
-    function handleTakeParcelByRider()
-    {
+    function handleTakeParcelByRider() {
 
     }
 
-    function handlePlaceParcelBySender()
-    {
+    function handlePlaceParcelBySender() {
 
     }
 
-    async function handleTakeParcelByReceiver()
-    {
+    async function handleTakeParcelByReceiver() {
         //updating compartment state to Empty
         setCompStateID(1);
         const values = {
@@ -132,9 +139,20 @@ function OpenCompartment(props) {
             .catch(err => {
                 console.error("Error while placing parcel: " + err);
             });
+        const values4 = {
+            parcelID: parcelid,
+            status: "parcelDelivered"
+        }
+        await axios.put('http://localhost:4001/updateStatus', values4)
+            .then(response => {
+                console.log(response.data.message);
+            })
+            .catch(err => {
+                throw (err);
+            });
 
         //sending SMS to receiver that parcel has been picked up
-        
+
         alert("Parcel has been picked up successfully");
     }
     return (
@@ -148,15 +166,15 @@ function OpenCompartment(props) {
             <h2>OTP: {otp}</h2>
             <h2>Purpose: {purpose}</h2>
             <button onClick={handleClosure}>Close Compartment</button>
-            {props.purpose === "receiving" ? 
+            {props.purpose === "receiving" ?
                 <div>
-                    {props.compstateid == 2 ? 
-                        <button onClick={handlePlaceParcelByRider}>Place Parcel</button> : 
+                    {props.compstateid == 2 ?
+                        <button onClick={handlePlaceParcelByRider}>Place Parcel</button> :
                         <button onClick={handleTakeParcelByReceiver}>Take Parcel</button>
                     }
-                </div> : 
+                </div> :
                 <div>
-                    {props.compstateid == 2 ? 
+                    {props.compstateid == 2 ?
                         <button onClick={handlePlaceParcelBySender}>Place Parcel</button> :
                         <button onClick={handleTakeParcelByRider}>Take Parcel</button>
                     }
